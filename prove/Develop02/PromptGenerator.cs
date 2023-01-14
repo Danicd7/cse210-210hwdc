f9c33b67e1f1ec7948f14d97c2ae460390b25d90
Danicd7 <danielsilva_88@outlook.com>
2023-01-15 00:19:32 +0100

    changes

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -4,21 +4,31 @@ using System;
 public class PromptMessage
 {
     static string chooseprompt()
-    {
+        {
         string prompt = "";
         Random randomGenerator = new Random();
         int PromptNumber = randomGenerator.Next(1, 5);
 
 
         
-        if (PromptNumber == 1)
+            if (PromptNumber == 1)
             {
-                string prompt = "Lower";
+                string prompt = "Positive Things";
             }
-            else
-            {
-                Console.WriteLine("You guesses it!");
+            if (PromptNumber == 2)
+        
+            { 
+                string prompt = "Things to change";
+            }
+            if (PromptNumber == 2)
+        
+            { 
+                string prompt = "Best moment of the day";
+            }
+            if (PromptNumber == 2)
+        
+            { 
+                string prompt = "Did you have any issues today? Tell me. “Who remembers the history won’t repeat it";
             }
         }
     }
-}
