6c235d2c44035b7da892ccce3f1eb66fbc5861ea
Danicd7 <danielsilva_88@outlook.com>
2023-02-15 21:08:24 +0100

    done

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,19 +7,26 @@ class Program
         //iterate through the list of videos and for each one, display
         //the title, author, lenght, number of comments, and list
         //out the comments for that video
-        comments v1 = new comments();
+        Comments commentsv1 = new Comments();
+        video v1 = new video();
         v1._title = "HOW TO DO A JIGSAW PUZZLE QUICKLY";
         v1._author = "Karen Puzzles";
         v1._length = 1792;
+
+        v1.DisplayInfo();
         
-        Video v2 = new Video();
+        Comments commentsv2 = new Comments();
+        video v2 = new video();
         v2._title = "One two three";
         v2._author = "Plain White Tees";
-        v2._lenght = 180;
+        v2._length = 180;
 
-        Video v3 = new Video();
+        Comments commentsv3 = new Comments();
+        video v3 = new video();
         v3._title = "The Biggest MYTH About Your Things";
         v3._author = "The Minimal Mom";
-        v3._lenght = 884;
+        v3._length = 884;
     }
+
+
 }
